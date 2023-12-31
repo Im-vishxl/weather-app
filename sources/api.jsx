164d9b0e7ef4cda3abdb80a6920178d6583087f8
Vishal <86511673+Im-vishxl@users.noreply.github.com>
2023-12-31 16:48:03 +0530

    Update api.jsx

--- sources/api.jsx
+++ sources/api.jsx
@@ -13,4 +13,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "947ed4ebff2d8a85d559f7970c9b7422";
+export const WEATHER_API_KEY = "key-here-also";
