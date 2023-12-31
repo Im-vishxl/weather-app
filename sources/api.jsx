928fd2a81d433c177421338978dc15bde51dfa6b
Im-vishxl <vishalkv214@gmail.com>
2023-12-31 16:10:13 +0530

    first commit

---
+++ sources/api.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7f6abd56e0msh14e56c577be54a0p18ccfdjsn079c04d6dd54',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "947ed4ebff2d8a85d559f7970c9b7422";
